ccae7233c83eee96d928680e252935b27b5107bc
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-06-15 12:45:34 -0500

    feat: Added front-page files (php, css) and added css file to theme_enqueue_scripts function

---
+++ front-page.php
@@ -0,0 +1,4 @@
+<?php
+    get_header();
+    echo "Response from front-page.php file";
+    get_footer();
--- functions.php
+++ functions.php
@@ -6,11 +6,13 @@
     }
 
     function theme_enqueue_scripts() {
+        // wp_enqueue_script('', get_template_directory_uri()."",array(), wp_get_theme()->get('Version'), true);
         wp_enqueue_style('style', get_stylesheet_uri(),array(), wp_get_theme()->get('Version'));
         wp_enqueue_style('header', get_template_directory_uri()."/css/header.css",array(), wp_get_theme()->get('Version'));
         wp_enqueue_style('footer', get_template_directory_uri()."/css/footer.css",array(), wp_get_theme()->get('Version'));
-        // wp_enqueue_style('', get_template_directory_uri()."",array(), wp_get_theme()->get('Version'));
-        // wp_enqueue_script('', get_template_directory_uri()."",array(), wp_get_theme()->get('Version'), true);
+        if (is_front_page()){
+            wp_enqueue_style('front-page', get_template_directory_uri()."/css/front-page.css",array(), wp_get_theme()->get('Version'));
+        }
     }
 
     function theme_register_nav_menus() {
