5dabb2eb39c7fcd100ab3413291411e584455d31
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-06-16 12:47:51 -0500

    feat(utils.php): Added documentation on each function for future use and reference

--- core/utils.php
+++ core/utils.php
@@ -1,5 +1,17 @@
 <?php
 
+    /**
+     * Add one element | item to menu
+     *
+     * NOTE:
+     *      This code was copied from stackoverflow.com
+     *      And this only add custom links elements | items to menu
+     *
+     * @param $id => Menu id
+     * @param $title => Title of element | item
+     * @param $url => URL for associate element | item
+     *
+    */
     function generate_theme_menu_item( $id, $title, $url ) {
         wp_update_nav_menu_item($id, 0, array(
             'menu-item-title'   =>  sprintf( __('%s', 'theme-inmobiliaria'), $title ),
@@ -10,6 +22,16 @@
         ) );
     }
 
+    /**
+     * Create a menu with given values
+     *
+     * NOTE:
+     *      This code was copied from stackoverflow.com
+     *
+     * @param $menu_name => Menu name
+     * @param $menu_items_array => Elements | Items of menu
+     * @param $location_target => Location to register the new menu
+     */
     function generate_theme_menu( $menu_name, $menu_items_array, $location_target ) {
         $menu_primary = $menu_name;
         wp_create_nav_menu( $menu_primary );
