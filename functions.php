3fda13c3146b040f54d2e8be13ea225b3e52122f
Israel Olvera (Skrigueztep) <skrigueztep@gmail.com>
2020-06-18 13:30:19 -0500

    feat(functions.php): Added utils.js to wp_enqueue_scripts, and added theme_add_module_type_scripts function linked to script_loader_tag

--- functions.php
+++ functions.php
@@ -12,6 +12,7 @@
         wp_enqueue_style('style', get_stylesheet_uri(),array(), wp_get_theme()->get('Version'));
         wp_enqueue_style('header', get_template_directory_uri()."/css/header.css",array(), wp_get_theme()->get('Version'));
         wp_enqueue_style('footer', get_template_directory_uri()."/css/footer.css",array(), wp_get_theme()->get('Version'));
+        wp_enqueue_script('theme-utils', get_template_directory_uri()."/js/utils.js",array(), wp_get_theme()->get('Version'), true);
         if (is_front_page()){
             wp_enqueue_style('front-page', get_template_directory_uri()."/css/front-page.css",array(), wp_get_theme()->get('Version'));
         }
@@ -40,6 +41,16 @@
         // generate_theme_menu('Menu name', array(), 'menu_identifier');
     }
 
+    /**
+     * Function to add type="module" on script tags, needed for enable imports from utils.js
+     */
+    function theme_add_module_type_scripts($tag, $handler, $src) {
+        // if ($handler !== 'theme-utils') return $tag; else return $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
+        $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
+        return $tag;
+    }
+
+    add_filter('script_loader_tag', 'theme_add_module_type_scripts', 10, 3);
     add_action('init', 'theme_setup');
     add_action('init', 'theme_register_nav_menus');
     add_action('wp_enqueue_scripts', 'theme_enqueue_scripts', 10);
